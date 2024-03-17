08b2089e1225287b9a11d994d2d18b2b627481fd
N3onNarwhal <averytlanier@icloud.com>
2024-03-17 18:10:37 -0500

    Lose screen added, accessed after player death

--- GameJam/Assets/Scripts/Player/PlayerHealth.cs
+++ GameJam/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -101,6 +102,7 @@ public class PlayerHealth : MonoBehaviour
 
     private void PlayerDeath()
     {
+        SceneManager.LoadScene("LoseScreen");
         /*
         //Debug.Log("You have died");
 
